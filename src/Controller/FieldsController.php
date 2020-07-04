8b775b40d7647acd2d583ec230445010cb469eda
hind benazzi <hind.benazzi@edu.uca.ma>
2020-07-04 21:54:38 +0200

    custemise filds pages

--- src/Controller/FieldsController.php
+++ src/Controller/FieldsController.php
@@ -5,18 +5,25 @@ use App\Entity\Fields;
 use App\Repository\FieldsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\FieldImage;
+use App\Repository\FieldImageRepository;
 
 class FieldsController extends AbstractController
 {
     /**
      * @Route("/field/{id}", name="fields")
      */
-    public function ShowField(Fields $field)
+    public function ShowField(Fields $field,EntityManagerInterface $em)
     {
-
+        $repo=$em->getRepository(FieldImage::class);
+        $fieldImages=$repo->findBy(array('fields'=>$field->getId()));
+        foreach($fieldImages as $key => $value){
+            $value->setImage(base64_encode(stream_get_contents($value->getImage())));
+        }
         
-            $field->setFieldPicture(base64_encode(stream_get_contents($field->getFieldPicture())));
+        $field->setFieldPicture(base64_encode(stream_get_contents($field->getFieldPicture())));
         
-        return $this->render('Concierge/field.html.twig',compact('field'));
+        return $this->render('Concierge/field.html.twig',['field'=>$field,'fieldImages'=>$fieldImages]);
     }
 }
