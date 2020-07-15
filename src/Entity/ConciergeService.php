621bf9f4a6a6c1283b85b086fd7ffbd221b870e7
hind benazzi <hind.benazzi@edu.uca.ma>
2020-07-15 16:08:18 +0100

    create concierge services

---
+++ src/Entity/ConciergeService.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ConciergeServiceRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ConciergeServiceRepository::class)
+ */
+class ConciergeService
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $ImageURL;
+
+    /**
+     * @ORM\OneToMany(targetEntity=ConciergeServiceRequete::class, mappedBy="Service")
+     */
+    private $conciergeServiceRequetes;
+
+    public function __construct()
+    {
+        $this->conciergeServiceRequetes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImageURL(): ?string
+    {
+        return $this->ImageURL;
+    }
+
+    public function setImageURL(?string $ImageURL): self
+    {
+        $this->ImageURL = $ImageURL;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|ConciergeServiceRequete[]
+     */
+    public function getConciergeServiceRequetes(): Collection
+    {
+        return $this->conciergeServiceRequetes;
+    }
+
+    public function addConciergeServiceRequete(ConciergeServiceRequete $conciergeServiceRequete): self
+    {
+        if (!$this->conciergeServiceRequetes->contains($conciergeServiceRequete)) {
+            $this->conciergeServiceRequetes[] = $conciergeServiceRequete;
+            $conciergeServiceRequete->setService($this);
+        }
+
+        return $this;
+    }
+
+    public function removeConciergeServiceRequete(ConciergeServiceRequete $conciergeServiceRequete): self
+    {
+        if ($this->conciergeServiceRequetes->contains($conciergeServiceRequete)) {
+            $this->conciergeServiceRequetes->removeElement($conciergeServiceRequete);
+            // set the owning side to null (unless already changed)
+            if ($conciergeServiceRequete->getService() === $this) {
+                $conciergeServiceRequete->setService(null);
+            }
+        }
+
+        return $this;
+    }
+}
